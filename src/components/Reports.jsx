b3c83f63b527c28c85e2a4e9c70eeb2f545b5e29
Prince Daksh <33217820-prince2113kumar@users.noreply.replit.com>
2025-09-27 07:47:47 +0000

    Update with no validation

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -25,66 +25,83 @@ const Reports = () => {
     <div className={`bg-dark ${styles.Container}`}>
       <h1 className={styles.heading}>Reports Generations</h1>
       <form className={styles.form} onSubmit={SubmitHandler}>
-        <label className={styles.Label}>Report Type</label>
-        <br />
-        <select name="Report Type" className={styles.Input}>
-          {Report.map((report, index) => {
-            return (
-              <option key={index} value={report}>
-                {report}
-              </option>
-            );
-          })}
-        </select>
-        <label className={styles.Label}>Species</label>
-        <br />
-        <select name="Species" className={styles.Input}>
-          {Species.map((specie, index) => {
-            return (
-              <option key={index} value={specie}>
-                {specie}
-              </option>
-            );
-          })}
-        </select>
-        <label className={styles.Label}>Conservational Status</label>
-        <br />
-        <select name="Status" className={styles.Input}>
-          {Conservational_Status.map((status, index) => {
-            return (
-              <option key={index} value={status}>
-                {status}
-              </option>
-            );
-          })}
-        </select>
-        <label className={styles.Label}>Start Date</label>
-        <br />
-        <input
-          name="Start Date"
-          type="date"
-          max={today}
-          className={styles.Input}
-        />
-        <label className={styles.Label}>End Date</label>
-        <br />
-        <input
-          name="End Date"
-          type="date"
-          max={today}
-          className={styles.Input}
-        />
-        <label className={styles.Label}>Export Format</label>
-        <br />
-        <select name="Export Format" className={styles.Input}>
-          {Format.map((format, index) => {
-            return (
-              <option key={index} value={format}>
-                {format}
-              </option>
-            );
-          })}
-        </select>
+        <div className={styles.InpCon}>
+          <label className={styles.Label}>Report Type</label>
+          <br />
+          <select name="Report Type" className={styles.Input}>
+            {Report.map((report, index) => {
+              return (
+                <option key={index} value={report}>
+                  {report}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className={styles.InpCon}>
+          <label className={styles.Label}>Species</label>
+          <br />
+          <select name="Species" className={styles.Input}>
+            {Species.map((specie, index) => {
+              return (
+                <option key={index} value={specie}>
+                  {specie}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className={styles.InpCon}>
+          <label className={styles.Label}>Conservational Status</label>
+          <br />
+          <select name="Status" className={styles.Input}>
+            {Conservational_Status.map((status, index) => {
+              return (
+                <option key={index} value={status}>
+                  {status}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className={styles.InpCon}>
+          <label className={styles.Label}>Start Date</label>
+          <br />
+          <input
+            name="Start Date"
+            type="date"
+            max={today}
+            className={styles.Input}
+          />
+        </div>
+
+        <div className={styles.InpCon}>
+          <label className={styles.Label}>End Date</label>
+          <br />
+          <input
+            name="End Date"
+            type="date"
+            max={today}
+            className={styles.Input}
+          />
+        </div>
+
+        <div className={styles.InpCon}>
+          <label className={styles.Label}>Export Format</label>
+          <br />
+          <select name="Export Format" className={styles.Input}>
+            {Format.map((format, index) => {
+              return (
+                <option key={index} value={format}>
+                  {format}
+                </option>
+              );
+            })}
+          </select>
+        </div>
         <div className={styles.btnCon}>
           <button type="submit" className={styles.btn}>
             Generate Report
