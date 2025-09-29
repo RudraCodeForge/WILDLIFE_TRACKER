24fec2468f3106d86df67f759c8aba990cd97bd3
Prince Daksh <jitandradaksh533@gmail.com>
2025-09-30 02:34:49 +0530

    Update UserList.jsx with new content

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,35 +1,41 @@
 import styles from "../styles/SpeciesList.module.css";
 import Species from "./Species";
-import {useLoaderData} from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import Spinner from "./Spinner";
+
 const UserList = () => {
   const users = useLoaderData();
-  console.log(users);
+
+  if (!users) {
+    return <Spinner />;
+  }
+
   return (
-    <div className={`bg-dark ${styles.container}`}>
+    <div className={`bg-dark ${styles.container}`}> 
       <h1 className={styles.Heading}>Manage Users</h1>
-      {users.map((user)=>{
-      return (
-        <Species
-          key={user.id}
-          Image={user.image}
-          Name={user.firstName}
-          ScientificName={user.email}
-          Status={user.role}
-          Link={`/manage-user/${user.id}`}
-          LinkName={"Manage User →"}
-        />
-      );
+      {users.map((user) => {
+        return (
+          <Species
+            key={user.id}
+            Image={user.image}
+            Name={user.firstName}
+            ScientificName={user.email}
+            Status={user.role}
+            Link={`/manage-user/${user.id}`}
+            LinkName={"Manage User →"}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
+
 export const Users = async () => {
   try {
     const res = await fetch('https://dummyjson.com/users');
     const data = await res.json();
-    return data.users;  // This will be returned properly
+    return data.users;
   } catch (error) {
-    console.error("Error fetching users:", error);
     return null;
   }
 };
