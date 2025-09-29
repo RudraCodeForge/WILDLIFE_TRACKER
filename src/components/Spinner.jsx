b04f05147d86f8c36860e8a54c064f19fcd297e6
Prince Daksh <jitandradaksh533@gmail.com>
2025-09-30 02:21:16 +0530

    Add reusable Spinner component for loading states

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Spinner.module.css";
+const Spinner = () => (
+  <div className={styles.spinnerContainer}>
+    <div className={styles.spinner}></div>
+  </div>
+);
+export default Spinner;
